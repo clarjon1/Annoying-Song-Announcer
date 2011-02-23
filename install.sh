b7af23e0cb7ed1c468dbb278c0af22057037a2cf
Flare183 <richardson183@bellsouth.net>
2011-02-23 11:01:12 -0500

    Started working on the installer script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# Bash installer for clarjon1 and Flare183's Annoying-Song-Announcer
+# Copyright 2011 clarjon1 and Flare183
+
+echo "Bash installer script for clarjon1 and Flare183's Annoying-Song-Announcer"
+echo "-------------------------------------------------------------------------"
+echo "Searching for clementine...."
+echo "(To begin with that is"
+
+CLEMENTINE = `ls /usr/bin/ | grep clementine`
+AMAROK = `ls /usr/bin/ | grep amarok"`
+
+if
+    $CLEMENTINE = "clementine"
+then
+    echo "I've see you are using clementine"
+    echo "Copying clementine asaconf file..."
+    cp -v $CURDIR/asaconf-clementine ~/.asaconf
+    echo "Done Copying"
+    echo "Copying the actual script to your xchat folder"
+    cp -v $CURDIR/curplay.pl ~/.xchat2/curplay.pl
+    echo "Done"
+    echo "Done installing, have a nice day"
+    break
+else
+    if
+	$AMAROK = "amarok"
+    then
+	
